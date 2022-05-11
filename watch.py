3a7c50a1db05e3ed34db6a0a885fa9e02fc3ef4c
davenpi <iandavenport@g.harvard.edu>
2022-05-11 08:08:44 -0400

    make vids of swing

--- watch.py
+++ watch.py
@@ -1,17 +1,18 @@
-from turtle import pos
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
-pos_arr = np.load("nbs/pos_arr.npy")
+pos_arr = np.load("nbs/positions_arr.npy")
+#pos_arr = np.load("nbs/pos_arr.npy")
+ref_pos_arr = np.load("nbs/ref_pos_arr.npy")
 
 
 def animate(i):
     ax.clear()
-
-    ax.plot([0, pos_arr[0][i]], [0, pos_arr[1][i]], "o-")
+    ax.plot([0, ref_pos_arr[0][i]], [0, ref_pos_arr[1][i]])
+    ax.plot([0, pos_arr[0][i]], [0, pos_arr[1][i]], "o")
     ax.set_xlim([-3, 3])
     ax.set_ylim([-3, 3])
     ax.set_title("Swinging over time")
