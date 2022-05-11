d17f24af29d3f9526f4004d4e440eef16cca21ea
davenpi <iandavenport@g.harvard.edu>
2022-05-11 13:43:31 -0400

    add drag model and solve

--- watch.py
+++ watch.py
@@ -2,11 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
+import argparse
 
-fig, ax = plt.subplots()
-pos_arr = np.load("nbs/positions_arr.npy")
-#pos_arr = np.load("nbs/pos_arr.npy")
-ref_pos_arr = np.load("nbs/ref_pos_arr.npy")
+parser = argparse.ArgumentParser(description="Process name")
+parser.add_argument("fname", type=str)
+args = parser.parse_args()
+
+fig, ax = plt.subplots(figsize=(10, 10))
+pos_arr = np.load("nbs/positions_arr_drag.npy")
+# pos_arr = np.load("nbs/pos_arr.npy")
+ref_pos_arr = np.load("nbs/ref_pos_arr_drag.npy")
 
 
 def animate(i):
@@ -21,8 +26,8 @@ def animate(i):
 
 
 # run the animation
-ani = FuncAnimation(fig, animate, frames=pos_arr[0].size, interval=500, repeat=False)
+ani = FuncAnimation(fig, animate, frames=pos_arr[0].size, interval=80, repeat=False)
 
-writervideo = animation.FFMpegWriter(fps=3)
-ani.save("swing_vid.mp4", writer=writervideo)
+writervideo = animation.FFMpegWriter(fps=8)
+ani.save(args.fname, writer=writervideo)
 plt.close()
